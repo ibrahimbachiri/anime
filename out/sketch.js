12d71f5422dfe02307e13e4c6d43c8df439b4417
ibrahimbachiri <133227314+ibrahimbachiri@users.noreply.github.com>
2023-12-13 12:50:26 +0100

    Update sketch.js

--- out/sketch.js
+++ out/sketch.js
@@ -1,24 +1,36 @@
 let walkk = [];
 let currentFrame = 0;
+let lastFrameTime = 0;
+let frameInterval = 100; // Set the desired frame interval in milliseconds
 
 function preload() {
   for (let i = 0; i < 10; i++) {
-    let fileName = 'walkk' + i + '.png'; // Update the path
+    let fileName = 'walkk' + i + '.png';
     walkk[i] = loadImage(fileName);
   }
 }
 
 function setup() {
   createCanvas(400, 400);
-    frameRate(100); // Set the frame rate to 10 frames per second (adjust as needed)
-
 }
 
 function draw() {
   background(220);
-  image(walkk[currentFrame], 0, 0);
-  currentFrame++;
-  if (currentFrame >= walkk.length) {
-    currentFrame = 0;
+
+  // Check if enough time has passed since the last frame
+  if (millis() - lastFrameTime > frameInterval) {
+    // Display the current frame
+    image(walkk[currentFrame], 0, 0);
+
+    // Move to the next frame
+    currentFrame++;
+
+    // If we've reached the end of the frames, loop back to the beginning
+    if (currentFrame >= walkk.length) {
+      currentFrame = 0;
+    }
+
+    // Update the last frame time
+    lastFrameTime = millis();
   }
 }
